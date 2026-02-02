475bf2ef974dfee633e5ed0dc78da052b8a5b0f9
mwangisam203 <mwangisam203@gmail.com>
2026-02-02 06:18:54 -0500

    init

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-class Dog:
-    def bark(self):
-        print("bark")
+class Student():
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade  # max grade = 100
 
-doggy = Dog()
-doggy.bark()
+    def get_grade(self):
+        return self.grade
+    
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    
+
+
+
+
+        
+        
